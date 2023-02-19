52eff3f84d91a781813accef42e17f95901f226e
LevTania <tanyalevandovska@gmail.com>
2023-02-19 16:06:16 +0200

    Added weather app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <body>
+          <div class="container + border">
+            <h1 id class="border2">
+              The forecast for the next 7 days
+            </h1>
+            <div class="container text-center">
+              <div class="row align-items-start">
+                <div class="col">
+                  <h2 id="city">Kyiv</h2>
+                </div>
+                <div class="col">
+                  <div class="iconTop">
+                    <img
+                      src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+                      alt="Cloudy"
+                      id="icon"
+                      class="float-left"
+                      width="80"
+                    />
+                  </div>
+                  <div class="col">
+                    <h2 id="time">-</h2>
+                    <span class="temperature" id="temperature">
+                      19
+                    </span>
+                    <span class="unit">
+                      <a href="#" id="celsius-link">
+                        °C
+                      </a>{" "}
+                      |{" "}
+                      <a href="#" id="fahrenheit-link">
+                        °F
+                      </a>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div class="col">
+                <form id="search-form">
+                  <div class="col-6">
+                    <input
+                      type="search"
+                      placeholder="Type a city for the forecast..."
+                      autofocus="on"
+                      autocomplete="off"
+                      id="city-input"
+                      class="form-control shadow-sm"
+                    />
+                  </div>
+                  <div class="col-3">
+                    <input
+                      type="submit"
+                      value="Search"
+                      class="form-control btn btn-primary shadow-sm w-100"
+                    />
+                  </div>
+                  <div class="days" id="forecast"></div>
+                  <div class="coded">
+                    <a
+                      href="https://github.com/LevTania/SheCodes_Plus_GitHub_Homework"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Open-source code
+                    </a>{" "}
+                    by{" "}
+                    <a
+                      href="https://www.linkedin.com/in/tetiana-levandovska-339181239/"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Tetiana Levandovska
+                    </a>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <script src="index.js"></script>
+        </body>
         <a
           className="App-link"
           href="https://reactjs.org"
